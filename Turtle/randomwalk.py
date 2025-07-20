90d87d4b5359fc880543dde5248f2abad54bf7df
jueunchoi <jueun1537@naver.com>
2025-07-20 16:55:25 +0900

    터틀GUI

---
+++ Turtle/randomwalk.py
@@ -0,0 +1,14 @@
+import turtle as t
+import random
+
+tim = t.Turtle()
+
+colors = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "slateGray", "SeaGreen"]
+directions = [0,90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random.choice(colors))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
